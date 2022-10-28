0a1b29073b6bd5ce0749a6f3b311e2d02cd80d4d
daniyal <dani.madmolil@gmail.com>
2022-10-28 17:35:40 +0330

    install nessessary libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-
-function App() {
+import React, { FC } from 'react';
+import { Container, CssBaseline, styled } from '@mui/material';
+interface AppProps {
+}
+const Root = styled(Container)(() => ({
+  background: "black",
+  maxWidth: "1920px"
+}))
+let App: FC<AppProps> = function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Root >
+      <CssBaseline />
+    </Root>
   );
 }
-
-export default App;
+// export default App;
+export default App
